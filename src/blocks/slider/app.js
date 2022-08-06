bdd7b0fb89c29b0867c39cd4414ac3d80f46737d
inc2734 <inc@2inc.org>
2022-08-06 14:06:15 +0900

    #10 Set rewind

--- src/blocks/slider/app.js
+++ src/blocks/slider/app.js
@@ -1,13 +1,14 @@
 import Swiper, { Navigation, Pagination } from 'swiper';
 
 document.addEventListener( 'DOMContentLoaded', () => {
-	new Swiper( '.unitone-slider__canvas', {
+	const unitoneSlider = new Swiper( '.unitone-slider__canvas', {
 		modules: [ Navigation, Pagination ],
 		followFinger: false,
 		// freeMode: true,
 		grabCursor: true,
 		slidesPerView: 'auto',
 		watchSlidesProgress: true,
+		rewind: true,
 		pagination: {
 			el: '.swiper-pagination',
 			clickable: true,

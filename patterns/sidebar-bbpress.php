3a37d6fa6cfc5d7132b6bcc3677de76f6d84096b
inc2734 <inc@2inc.org>
2022-11-21 21:44:16 +0900

    fix #66

---
+++ patterns/sidebar-bbpress.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Title: Sidebar (for bbPress)
+ * Slug: unitone/sidebar-bbpress
+ * inserter: false
+ */
+?>
+<!-- wp:unitone/stack -->
+<div data-unitone-layout="stack">
+	<!-- wp:unitone-bbpress-integrator/login {"title":"<?php esc_html_e( 'Login', 'unitone' ); ?>"} /-->
+	<!-- wp:unitone-bbpress-integrator/stats {"title":"<?php esc_html_e( 'Statistics', 'unitone' ); ?>"} /-->
+	<!-- wp:unitone-bbpress-integrator/topics {"title":"<?php esc_html_e( 'Recent Topics', 'unitone' ); ?>","showDate":true,"showUser":true} /-->
+	<!-- wp:unitone-bbpress-integrator/search {"title":"<?php esc_html_e( 'Forum Search', 'unitone' ); ?>"} /-->
+</div>
+<!-- /wp:unitone/stack -->

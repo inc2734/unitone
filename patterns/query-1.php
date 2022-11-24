78e40c9a8a0d3345b69fea927f0dde57ab5f6496
inc2734 <inc@2inc.org>
2022-11-24 22:40:54 +0900

    4.3.1

--- patterns/query-1.php
+++ patterns/query-1.php
@@ -11,8 +11,8 @@
 	<!-- wp:post-template -->
 		<!-- wp:unitone/stack {"unitone":{"gap":"-2"}} -->
 		<div data-unitone-layout="stack -gap:-2">
-			<!-- wp:unitone/with-sidebar {"sidebar":"left"} -->
-			<div data-unitone-layout="with-sidebar -sidebar:left">
+			<!-- wp:unitone/with-sidebar {"sidebar":"left","sidebarWidth":"6em"} -->
+			<div data-unitone-layout="with-sidebar -sidebar:left" style="--unitone--sidebar-width:6em">
 				<!-- wp:unitone/with-sidebar-content -->
 				<div data-unitone-layout="with-sidebar__content">
 					<!-- wp:post-date {"fontSize":"unitone-xs"} /-->

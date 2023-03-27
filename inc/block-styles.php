7537cda020915f6f7d145b9723d3b13c6c9becdc
inc2734 <inc@2inc.org>
2023-03-27 23:48:53 +0900

    Fix #74

--- inc/block-styles.php
+++ inc/block-styles.php
@@ -15,6 +15,14 @@ function unitone_register_block_styles() {
 	);
 
 	register_block_style(
+		'core/post-terms',
+		array(
+			'name'  => 'outline',
+			'label' => __( 'Outline', 'unitone' ),
+		)
+	);
+
+	register_block_style(
 		'core/query',
 		array(
 			'name'  => 'underline',
@@ -91,35 +99,35 @@ add_action( 'init', 'unitone_register_block_styles', 9 );
 /**
  * Add CSS for .is-style-badge.
  */
-foreach ( array( 'wp_enqueue_scripts', 'enqueue_block_editor_assets' ) as $hook ) {
-	add_action(
-		$hook,
-		function() {
-			$color_settings = WP_Theme_JSON_Resolver::get_merged_data()->get_raw_data()['settings']['color'];
-			$duotone        = ! empty( $color_settings['duotone'] ) ? $color_settings['duotone'] : array();
-			$gradients      = ! empty( $color_settings['gradients'] ) ? $color_settings['gradients'] : array();
-			$palette        = ! empty( $color_settings['palette'] ) ? $color_settings['palette'] : array();
+// foreach ( array( 'wp_enqueue_scripts', 'enqueue_block_editor_assets' ) as $hook ) {
+// 	add_action(
+// 		$hook,
+// 		function() {
+// 			$color_settings = WP_Theme_JSON_Resolver::get_merged_data()->get_raw_data()['settings']['color'];
+// 			$duotone        = ! empty( $color_settings['duotone'] ) ? $color_settings['duotone'] : array();
+// 			$gradients      = ! empty( $color_settings['gradients'] ) ? $color_settings['gradients'] : array();
+// 			$palette        = ! empty( $color_settings['palette'] ) ? $color_settings['palette'] : array();
 
-			$categories = array(
-				'duotone'   => $duotone,
-				'gradients' => $gradients,
-				'color'     => $palette,
-			);
+// 			$categories = array(
+// 				'duotone'   => $duotone,
+// 				'gradients' => $gradients,
+// 				'color'     => $palette,
+// 			);
 
-			$css = '';
-			foreach ( $categories as $category_name => $category ) {
-				foreach ( $category as $type ) {
-					foreach ( $type as $color ) {
-						$slug = str_replace( '/', '-', $color['slug'] );
-						$css .= sprintf(
-							'.is-style-badge.has-%1$s-background-color a {background-color: var(--wp--preset--%2$s--%1$s)}',
-							$slug,
-							$category_name
-						);
-					}
-				}
-			}
-			wp_add_inline_style( 'unitone', $css );
-		}
-	);
-}
+// 			$css = '';
+// 			foreach ( $categories as $category_name => $category ) {
+// 				foreach ( $category as $type ) {
+// 					foreach ( $type as $color ) {
+// 						$slug = str_replace( '/', '-', $color['slug'] );
+// 						$css .= sprintf(
+// 							'.is-style-badge.has-%1$s-background-color a {background-color: var(--wp--preset--%2$s--%1$s)}',
+// 							$slug,
+// 							$category_name
+// 						);
+// 					}
+// 				}
+// 			}
+// 			wp_add_inline_style( 'unitone', $css );
+// 		}
+// 	);
+// }

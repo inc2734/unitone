a3bd3b0924c53d74e6d2e403c20c4345e654df0c
inc2734 <inc@2inc.org>
2023-10-26 21:54:10 +0900

    Some fixes

--- patterns/private/template/search/main/one-column-page-header-image.php
+++ patterns/private/template/search/main/one-column-page-header-image.php
@@ -8,7 +8,11 @@
 		<div data-unitone-layout="stack">
 			<!-- wp:unitone/text {"unitone":{"maxWidth":"var(--unitone--container-max-width)","gutters":"root"}} -->
 			<div data-unitone-layout="text">
-				<!-- wp:search {"label":"Search","showLabel":false,"width":100,"widthUnit":"%","buttonText":"Search","buttonUseIcon":true} /-->
+				<!-- wp:unitone/container {"unitone":{"gutters":"0","blockAlign":"center","maxWidth":"1024px"}} -->
+				<div data-unitone-layout="container">
+					<!-- wp:search {"label":"Search","showLabel":false,"width":100,"widthUnit":"%","buttonText":"Search","buttonUseIcon":true} /-->
+				</div>
+				<!-- /wp:unitone/container -->
 
 				<!-- wp:pattern {"slug":"unitone/query/4"} /-->
 			</div>
--- patterns/private/template/search/main/right-sidebar.php
+++ patterns/private/template/search/main/right-sidebar.php
@@ -16,7 +16,11 @@
 						<div data-unitone-layout="stack">
 							<!-- wp:unitone/text {"unitone":{"maxWidth":"100%"}} -->
 							<div data-unitone-layout="text">
-								<!-- wp:search {"label":"Search","showLabel":false,"width":100,"widthUnit":"%","buttonText":"Search","buttonUseIcon":true} /-->
+								<!-- wp:unitone/container {"unitone":{"gutters":"0","blockAlign":"center","maxWidth":"1024px"}} -->
+								<div data-unitone-layout="container">
+									<!-- wp:search {"label":"Search","showLabel":false,"width":100,"widthUnit":"%","buttonText":"Search","buttonUseIcon":true} /-->
+								</div>
+								<!-- /wp:unitone/container -->
 
 								<!-- wp:pattern {"slug":"unitone/query/4"} /-->
 							</div>

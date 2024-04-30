87bc7ac19536fb8a7fd18c38ff995cb89bb6bac8
inc2734 <inc@2inc.org>
2024-04-30 16:56:44 +0900

    ヘッダー（2行）の構造をグリッドブロックを使う形に変更

--- patterns/header-2rows.php
+++ patterns/header-2rows.php
@@ -10,22 +10,18 @@
 <div data-unitone-layout="container">
 	<!-- wp:unitone/gutters {"unitone":{"padding":"-1"}} -->
 	<div data-unitone-layout="gutters">
-		<!-- wp:unitone/with-sidebar {"sidebarWidth":"max(24px,(((601px - var(--unitone--gutters) * 2 - 100%) * 999) * -1))","unitone":{"alignItems":"center","gap":{"column":"-1","row":"-2"}}} -->
-		<div data-unitone-layout="with-sidebar -sidebar:right" style="--unitone--sidebar-width:max(24px,(((601px - var(--unitone--gutters) * 2 - 100%) * 999) * -1))">
-			<!-- wp:unitone/with-sidebar-content -->
-			<div data-unitone-layout="with-sidebar__content">
+		<!-- wp:unitone/grid {"columnsOption":"columns","smColumnsOption":"free","smColumns":2,"smGridTemplateColumns":"1fr auto","unitone":{"gap":{"row":"-2","column":"-1"}}} -->
+		<div class="unitone-grid" style="--unitone--columns:1;--unitone--sm-grid-template-columns:1fr auto;--unitone--rows:1" data-unitone-layout="-columns:columns -columns:sm:free -rows:rows">
+			<!-- wp:unitone/stack {"unitone":{"gap":"0","alignSelf":"stretch","justifySelf":"stretch","gridColumn":"auto","gridRow":"auto"}} -->
+			<div data-unitone-layout="stack">
 				<!-- wp:site-logo /-->
 				<!-- wp:site-title /-->
 			</div>
-			<!-- /wp:unitone/with-sidebar-content -->
+			<!-- /wp:unitone/stack -->
 
-			<!-- wp:unitone/with-sidebar-content -->
-			<div data-unitone-layout="with-sidebar__content">
-				<!-- wp:navigation {"layout":{"type":"flex","justifyContent":"left","orientation":"horizontal"},"style":{"typography":{"fontWeight":"600"}},"fontSize":"unitone-s","className":"is-style-unitone"} /-->
-			</div>
-			<!-- /wp:unitone/with-sidebar-content -->
+			<!-- wp:navigation {"layout":{"type":"flex","justifyContent":"left","orientation":"horizontal"},"style":{"typography":{"fontWeight":"600"}},"fontSize":"unitone-s","className":"is-style-unitone"} /-->
 		</div>
-		<!-- /wp:unitone/with-sidebar -->
+		<!-- /wp:unitone/grid -->
 	</div>
 	<!-- /wp:unitone/gutters -->
 </div>

1b9258cf5eceeb5722593a5eea0015c4029bf4a8
inc2734 <inc@2inc.org>
2024-04-30 16:57:27 +0900

    左ヘッダーレイアウトをグリッドブロックを使う形に変更

--- patterns/header-vertical.php
+++ patterns/header-vertical.php
@@ -9,18 +9,18 @@
 <div data-unitone-layout="container">
 	<!-- wp:unitone/gutters {"unitone":{"padding":"-1"}} -->
 	<div data-unitone-layout="gutters">
-		<!-- wp:unitone/both-sides {"unitone":{"alignItems":"center","gap":-1}} -->
-		<div data-unitone-layout="both-sides">
-			<!-- wp:unitone/stack {"unitone":{"gap":"0"}} -->
+		<!-- wp:unitone/grid {"columnsOption":"columns","smColumnsOption":"free","smGridTemplateColumns":"1fr auto","unitone":{"gap":{"row":"1","column":"-1"}}} -->
+		<div class="unitone-grid" style="--unitone--columns:1;--unitone--sm-grid-template-columns:1fr auto;--unitone--rows:1" data-unitone-layout="-columns:columns -columns:sm:free -rows:rows">
+			<!-- wp:unitone/stack {"unitone":{"gap":"0","alignSelf":"center"}} -->
 			<div data-unitone-layout="stack">
 				<!-- wp:site-logo /-->
 				<!-- wp:site-title /-->
 			</div>
 			<!-- /wp:unitone/stack -->
 
-			<!-- wp:navigation {"layout":{"type":"flex","orientation":"vertical"},"style":{"typography":{"fontWeight":"600"}},"fontSize":"unitone-s","className":"is-style-unitone"} /-->
+			<!-- wp:navigation {"layout":{"type":"flex","orientation":"vertical"},"style":{"typography":{"fontWeight":"600"}},"fontSize":"unitone-s","className":"is-style-unitone","unitone":{"alignSelf":"center"}} /-->
 		</div>
-		<!-- /wp:unitone/both-sides -->
+		<!-- /wp:unitone/grid -->
 	</div>
 	<!-- /wp:unitone/gutters -->
 </div>

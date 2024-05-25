0f6190466cd33c08fc83aaded97c25e41ed9a1bd
inc2734 <inc@2inc.org>
2024-05-26 07:05:02 +0900

    fix #323

--- src/js/editor/hooks/components.js
+++ src/js/editor/hooks/components.js
@@ -23,6 +23,12 @@ function Controls( { isMixed, value, onChange, marks, options } ) {
 				min={ marks[ 0 ]?.value || marks[ 1 ]?.value }
 				max={ marks[ marks.length - 1 ]?.value }
 				marks={ marks }
+				renderTooltipContent={ ( _value ) =>
+					options.filter(
+						( option ) =>
+							parseInt( option.value ) === parseInt( _value )
+					)?.[ 0 ]?.label ?? _value
+				}
 				hideLabelFromVision
 				__nextHasNoMarginBottom
 			/>
@@ -52,44 +58,44 @@ export function SpacingSizeControl( {
 			value: undefined,
 		},
 		{
-			label: '-2',
-			value: '-2',
+			label: 'XS',
+			value: -2,
 		},
 		{
-			label: '-1',
-			value: '-1',
+			label: 'S',
+			value: -1,
 		},
 		{
 			label: __( 'None', 'unitone' ),
-			value: '0',
+			value: 0,
 		},
 		{
-			label: '1',
-			value: '1',
+			label: 'M',
+			value: 1,
 		},
 		{
-			label: '2',
-			value: '2',
+			label: 'L',
+			value: 2,
 		},
 		{
-			label: '3',
-			value: '3',
+			label: '2XL',
+			value: 3,
 		},
 		{
-			label: '4',
-			value: '4',
+			label: '3XL',
+			value: 4,
 		},
 		{
-			label: '5',
-			value: '5',
+			label: '4XL',
+			value: 5,
 		},
 		{
-			label: '6',
-			value: '6',
+			label: '5XL',
+			value: 6,
 		},
 		{
-			label: '7',
-			value: '7',
+			label: '6XL',
+			value: 7,
 		},
 	];
 

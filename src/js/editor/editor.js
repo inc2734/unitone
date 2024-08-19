0d87077374257f9c03301f13cf6f239163f5be8e
inc2734 <inc@2inc.org>
2024-08-20 01:10:23 +0900

    fix #372

--- src/js/editor/editor.js
+++ src/js/editor/editor.js
@@ -6,4 +6,3 @@ import './wp-blocks/table';
 import './plugins/lineage-toolbar/index';
 import './plugins/highlight-selected-block/index';
 import './plugins/block-outline-toolbar/index';
-import './plugins/template-part-replace/index';

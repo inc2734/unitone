38a323b6dd5fbc02ffd14a767a7174697635c5e7
inc2734 <inc@2inc.org>
2024-11-05 10:32:26 +0900

    #408 誤って削除してしまったパターンを復活

---
+++ patterns/template-single-right-sidebar-page-header-image.php
@@ -0,0 +1,30 @@
+<?php
+/**
+ * Title: Single Posts: Right Sidebar / Page Header (Image)
+ * Slug: unitone/template/single/right-sidebar-page-header-image
+ * Categories: unitone-templates
+ * Template Types: single
+ * Inserter: no
+ */
+?>
+<!-- wp:unitone/cover {"unitone":{"gap":"0","padding":"0"}} -->
+<div data-unitone-layout="cover">
+	<!-- wp:unitone/cover-content {"position":"top"} -->
+	<div data-unitone-layout="cover__content -valign:top">
+		<!-- wp:template-part {"slug":"header","tagName":"header","className":"site-header"} /-->
+	</div>
+	<!-- /wp:unitone/cover-content -->
+
+	<!-- wp:unitone/cover-content {"fill":true,"position":"center"} -->
+	<div data-unitone-layout="cover__content -fill -valign:center">
+		<!-- wp:pattern {"slug":"unitone/template/single/main/right-sidebar-page-header-image"} /-->
+	</div>
+	<!-- /wp:unitone/cover-content -->
+
+	<!-- wp:unitone/cover-content {"position":"bottom"} -->
+	<div data-unitone-layout="cover__content -valign:bottom">
+		<!-- wp:template-part {"slug":"footer","tagName":"footer","className":"site-footer"} /-->
+	</div>
+	<!-- /wp:unitone/cover-content -->
+</div>
+<!-- /wp:unitone/cover -->

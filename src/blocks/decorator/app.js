d7106100a62e71bab0fe3600c3ad34a6852009d9
inc2734 <inc@2inc.org>
2024-12-27 12:56:02 +0900

    fix #459

--- src/blocks/decorator/app.js
+++ src/blocks/decorator/app.js
@@ -30,7 +30,21 @@ document.addEventListener( 'DOMContentLoaded', () => {
 
 				up = +new Date();
 				if ( up - down < 200 ) {
-					link.click();
+					const pressedKeys =
+						event.shiftKey || event.ctrlKey || event.metaKey;
+					if ( pressedKeys ) {
+						const originalTarget = link.getAttribute( 'target' );
+						const originalRel = link.getAttribute( 'rel' );
+						link.setAttribute( 'target', '_blank' );
+						link.setAttribute( 'rel', 'noopener noreferrer' );
+
+						link.click();
+
+						link.setAttribute( 'target', originalTarget || '' );
+						link.setAttribute( 'rel', originalRel || '' );
+					} else {
+						link.click();
+					}
 				}
 			} );
 		}

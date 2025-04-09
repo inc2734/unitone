17138930305896a5d8e80776bca251ef57565235
inc2734 <inc@2inc.org>
2025-04-09 10:56:43 +0900

    #537 __unstableUseCompositeState

--- src/blocks/pattern-inserter/inserter-listbox/index.js
+++ src/blocks/pattern-inserter/inserter-listbox/index.js
@@ -1,30 +1,24 @@
 /**
- * @see https://github.com/WordPress/gutenberg/blob/7f7a58a154084df65c493e58365402ac89fb6950/packages/block-editor/src/components/inserter-listbox/index.js
+ * @see https://github.com/WordPress/gutenberg/blob/7cf62f7e55edab0386f01795f7217c9dc06ba032/packages/block-editor/src/components/inserter-listbox/index.js
  */
 
 /**
  * WordPress dependencies
  */
-import { __unstableUseCompositeState as useCompositeState } from '@wordpress/components';
+import { Composite } from '@wordpress/components';
 
 /**
  * Internal dependencies
  */
-import InserterListboxContext from './context';
-
 export { default as InserterListboxGroup } from './group';
 export { default as InserterListboxRow } from './row';
 export { default as InserterListboxItem } from './item';
 
 function InserterListbox( { children } ) {
-	const compositeState = useCompositeState( {
-		shift: true,
-		wrap: 'horizontal',
-	} );
 	return (
-		<InserterListboxContext.Provider value={ compositeState }>
+		<Composite focusShift focusWrap="horizontal" render={ <></> }>
 			{ children }
-		</InserterListboxContext.Provider>
+		</Composite>
 	);
 }
 

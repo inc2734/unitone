c8b500a1332dd7847211187a4592c6c519d64676
inc2734 <inc@2inc.org>
2025-06-05 09:09:30 +0900

    Add front page: fixed header template

---
+++ patterns/template-front-page-fixed-header.php
@@ -0,0 +1,30 @@
+<?php
+/**
+ * Title: Front Page: Fixed Header
+ * Slug: unitone/template/front-page/fixed-header
+ * Categories: unitone-templates
+ * Template Types: front-page
+ * Inserter: no
+ */
+?>
+<!-- wp:unitone/cover {"unitone":{"gap":"0","padding":"0"}} -->
+<div data-unitone-layout="cover">
+	<!-- wp:unitone/cover-content {"position":"top","unitone":{"position":{"position":"fixed","zIndex":"2","left":"0px","right":"0px","top":"var(--wp-admin--admin-bar--height, 0px)"}}} -->
+	<div data-unitone-layout="cover__content -valign:top">
+		<!-- wp:template-part {"slug":"header","tagName":"header","className":"site-header"} /-->
+	</div>
+	<!-- /wp:unitone/cover-content -->
+
+	<!-- wp:unitone/cover-content {"fill":true,"position":"center"} -->
+	<div data-unitone-layout="cover__content -fill -valign:center">
+		<!-- wp:pattern {"slug":"unitone/template/front-page/main/blank"} /-->
+	</div>
+	<!-- /wp:unitone/cover-content -->
+
+	<!-- wp:unitone/cover-content {"position":"bottom"} -->
+	<div data-unitone-layout="cover__content -valign:bottom">
+		<!-- wp:template-part {"slug":"footer","tagName":"footer","className":"site-footer"} /-->
+	</div>
+	<!-- /wp:unitone/cover-content -->
+</div>
+<!-- /wp:unitone/cover -->

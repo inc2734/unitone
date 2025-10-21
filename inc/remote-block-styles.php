f5b740b71d8563f7354061f820c012a5212c4fd9
inc2734 <inc@2inc.org>
2025-10-21 14:05:05 +0900

    fix #667

--- inc/remote-block-styles.php
+++ inc/remote-block-styles.php
@@ -70,6 +70,69 @@ function unitone_get_premium_remote_block_styles() {
 }
 
 /**
+ * Enqueue remote block styles assets for front.
+ *
+ * [
+ *     'core/paragraph' => [
+ *         'default' => 'color:red;',
+ *         'outline' => 'border:1px solid #000;',
+ *     ],
+ *     'core/image' => [
+ *         'rounded' => 'border-radius:50%;',
+ *     ],
+ * ]
+ *
+ * @param array $_map Map of block type.
+ */
+function _unitone_enqueue_remote_block_styles_for_front( $_map ) {
+	static $map  = $_map;
+	static $hits = array();
+
+	add_filter(
+		'render_block_data',
+		function ( $parsed_block ) use ( &$map, &$hits ) {
+			if ( ! $parsed_block['blockName'] ) {
+				return $parsed_block;
+			}
+
+			$block_type = $parsed_block['blockName'];
+			$class_name = (string) ( $parsed_block['attrs']['className'] ?? '' );
+			if ( ! $class_name || empty( $map[ $block_type ] ) ) {
+				return $parsed_block;
+			}
+
+			foreach ( $map[ $block_type ] as $name => $css ) {
+				if ( false !== strpos( $class_name, 'is-style-' . $name ) ) {
+					$hits[ $name ] = $block_type;
+				}
+			}
+
+			return $parsed_block;
+		}
+	);
+
+	add_action(
+		'wp_enqueue_scripts',
+		function () use ( &$map, &$hits ) {
+			global $wp_styles;
+
+			if ( ! $hits ) {
+				return;
+			}
+
+			foreach ( $hits as $name => $block_type ) {
+				$deps_handle = generate_block_asset_handle( $block_type, 'style' );
+				$handle      = $name . '-' . $block_type;
+
+				wp_register_style( $handle, false, array( $deps_handle ) ); // phpcs:ignore
+				wp_add_inline_style( $handle, $map[ $block_type ][ $name ] );
+				wp_enqueue_style( $handle );
+			}
+		}
+	);
+}
+
+/**
  * Register remote block styles.
  * This requires a valid license key.
  */
@@ -107,21 +170,47 @@ function unitone_register_remote_block_styles() {
 		}
 	}
 
+	$get_block_types_from_remote_block_styles_block_type = static function ( $block_types ) {
+		return array_map(
+			function ( $block_type ) {
+				return str_replace( array( 'core-', 'unitone-' ), array( 'core/', 'unitone/' ), $block_type );
+			},
+			$block_types
+		);
+	};
+
+	$on_demand = apply_filters(
+		'unitone_should_load_block_styles_assets_on_demand',
+		wp_should_load_block_assets_on_demand()
+	);
+
+	if ( ! $on_demand || is_admin() ) {
+		foreach ( $block_styles as $style ) {
+			if ( ! $style['name'] || ! $style['label'] || ! $style['blockTypes'] ) {
+				continue;
+			}
+
+			$block_types = $get_block_types_from_remote_block_styles_block_type( $style['blockTypes'] );
+
+			register_block_style( $block_types, (array) $style );
+		}
+		return;
+	}
+
+	$map = array();
 	foreach ( $block_styles as $style ) {
-		if ( ! $style['name'] || ! $style['label'] || ! $style['blockTypes'] ) {
+		if ( ! $style['name'] || ! $style['inline_style'] || ! $style['blockTypes'] ) {
 			continue;
 		}
 
-		$block_types = array_filter(
-			array_map(
-				function ( $block_type ) {
-					return str_replace( array( 'core-', 'unitone-' ), array( 'core/', 'unitone/' ), $block_type );
-				},
-				$style['blockTypes']
-			)
-		);
+		$name        = sanitize_key( $style['name'] );
+		$block_types = $get_block_types_from_remote_block_styles_block_type( $style['blockTypes'] );
 
-		register_block_style( $block_types, (array) $style );
+		foreach ( $block_types as $block_type ) {
+			$map[ $block_type ][ $name ] = $css;
+		}
 	}
+
+	_unitone_enqueue_remote_block_styles_for_front( $map );
 }
 add_action( 'init', 'unitone_register_remote_block_styles', 9 );

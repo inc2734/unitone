86e5c493f6462364919c9bc46354fad83d13e1ca
inc2734 <inc@2inc.org>
2025-11-10 18:53:22 +0900

    #672 子ブロック対応

--- src/blocks/grid/hooks.js
+++ src/blocks/grid/hooks.js
@@ -11,7 +11,70 @@ export const useResponsiveGridCSS = ( {
 		const buildCSS = ( breakpoint, size ) => {
 			const prefix = `--unitone--${ size }`;
 
-			return `@media not all and (min-width: ${ breakpoint }) { ${ selector } > * { --unitone--active--grid-column: var(${ prefix }-grid-column); --unitone--active--grid-row: var(${ prefix }-grid-row); } ${ selector }[data-unitone-layout~="-columns\\:${ size }\\:columns"] { --unitone--active--columns: var(${ prefix }-columns); } ${ selector }[data-unitone-layout~="-columns\\:${ size }\\:min"] { --unitone--active--column-min-width: var(${ prefix }-column-min-width); } ${ selector }[data-unitone-layout~="-columns\\:${ size }\\:free"] { --unitone--active--grid-template-columns: var(${ prefix }-grid-template-columns); } ${ selector }[data-unitone-layout~="-rows\\:${ size }\\:rows"] { --unitone--active--rows: var(${ prefix }-rows); } ${ selector }[data-unitone-layout~="-rows\\:${ size }\\:free"] { --unitone--active--grid-template-rows: var(${ prefix }-grid-template-rows); } }`;
+			return `@media not all and (min-width: ${ breakpoint }) {
+				${ selector }[data-unitone-layout~="-columns\\:${ size }\\:columns"] {
+					--unitone--active--columns: var(${ prefix }-columns);
+				}
+
+				${ selector }[data-unitone-layout~="-columns\\:${ size }\\:min"] {
+					--unitone--active--column-min-width: var(${ prefix }-column-min-width);
+				}
+
+				${ selector }[data-unitone-layout~="-columns\\:${ size }\\:free"] {
+					--unitone--active--grid-template-columns: var(${ prefix }-grid-template-columns);
+				}
+
+				${ selector }[data-unitone-layout~="-rows\\:${ size }\\:rows"] {
+					--unitone--active--rows: var(${ prefix }-rows);
+				}
+
+				${ selector }[data-unitone-layout~="-rows\\:${ size }\\:free"] {
+					--unitone--active--grid-template-rows: var(${ prefix }-grid-template-rows);
+				}
+
+				${ selector } > * {
+					--unitone--active--grid-column: var(${ prefix }-grid-column);
+					--unitone--active--grid-row: var(${ prefix }-grid-row);
+					--unitone--active--align-self: var(${ prefix }-align-self);
+					--unitone--active--justify-self: var(${ prefix }-justify-self);
+				}
+
+				${ selector } > [data-unitone-layout~="-align-self\:${ size }\:start"] {
+					${ prefix }-align-self: start;
+					margin-top: 0;
+				}
+
+				${ selector } > [data-unitone-layout~="-align-self\:${ size }\:end"] {
+					${ prefix }-align-self: end;
+					margin-bottom: 0;
+				}
+
+				${ selector } > [data-unitone-layout~="-align-self\:${ size }\:center"] {
+					${ prefix }-align-self: center;
+				}
+
+				${ selector } > [data-unitone-layout~="-align-self\:${ size }\:stretch"] {
+					${ prefix }-align-self: stretch;
+				}
+
+				${ selector } > [data-unitone-layout~="-justify-self\:${ size }\:start"] {
+					${ prefix }-justify-self: start;
+					margin-left: 0;
+				}
+
+				${ selector } > [data-unitone-layout~="-justify-self\:${ size }\:end"] {
+					${ prefix }-justify-self: end;
+					margin-right: 0;
+				}
+
+				${ selector } > [data-unitone-layout~="-justify-self\:${ size }\:center"] {
+					${ prefix }-justify-self: center;
+				}
+
+				${ selector } > [data-unitone-layout~="-justify-self\:${ size }\:stretch"] {
+					${ prefix }-justify-self: stretch;
+				}
+			}`;
 		};
 
 		return [

263ab38cfd444c42a8713fe08fc6080c92c4f3b7
inc2734 <inc@2inc.org>
2025-12-16 23:02:57 +0900

    fix #701

--- patterns/header/header-vertical-thin.php
+++ patterns/header/header-vertical-thin.php
@@ -10,15 +10,16 @@
 	<div data-unitone-layout="gutters">
 		<div data-unitone-layout="container">
 			<div data-unitone-layout="stack">
-				<!-- wp:unitone/grid {"columnsOption":"columns","smColumnsOption":"free","smGridTemplateColumns":"1fr auto","unitone":{"gap":{"row":"3","column":"-1"},"justifyItems":"center"}} -->
+				<!-- wp:unitone/grid {"columnsOption":"columns","smColumnsOption":"free","smGridTemplateColumns":"1fr auto","unitone":{"gap":{"row":"1","column":"-1"},"justifyItems":"stretch","alignItems":"center"}} -->
 				<div class="unitone-grid" style="--unitone--sm-grid-template-columns:1fr auto;--unitone--rows:1" data-unitone-layout="-columns:columns -columns:sm:free -rows:rows">
-					<!-- wp:unitone/stack {"unitone":{"gap":"0","alignSelf":"center"}} -->
-					<div data-unitone-layout="stack"><!-- wp:site-logo /-->
+					<!-- wp:unitone/stack {"unitone":{"gap":"0","justifySelf":{"lg":"center","sm":"start"}}} -->
+					<div data-unitone-layout="stack">
+						<!-- wp:site-logo /-->
 						<!-- wp:site-title /-->
 					</div>
 					<!-- /wp:unitone/stack -->
 
-					<!-- wp:navigation {"overlayMenu":"always","className":"is-style-unitone-accordion","style":{"typography":{"fontWeight":"600","fontStyle":"normal"}},"fontSize":"unitone-s","layout":{"type":"flex","orientation":"vertical"}} -->
+					<!-- wp:navigation {"overlayMenu":"always","unitone":{"justifySelf":{"lg":"center","sm":"auto"}},"className":"is-style-unitone-accordion","style":{"typography":{"fontWeight":"600","fontStyle":"normal"}},"fontSize":"unitone-s","layout":{"type":"flex","orientation":"vertical"}} -->
 						<!-- wp:page-list /-->
 					<!-- /wp:navigation -->
 				</div>

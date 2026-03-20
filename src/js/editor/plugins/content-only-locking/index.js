af6558131271c15fc33dfea0084e644f79f29ff2
inc2734 <inc@2inc.org>
2026-03-20 23:55:08 +0900

    #751 Some fixes

--- src/js/editor/plugins/content-only-locking/index.js
+++ src/js/editor/plugins/content-only-locking/index.js
@@ -5,6 +5,34 @@ import { registerPlugin } from '@wordpress/plugins';
 import { lockOutline, unlock } from '@wordpress/icons';
 import { __ } from '@wordpress/i18n';
 
+const clearEditableOutlineClass = ( clientIds ) => {
+	if ( 'undefined' === typeof document ) {
+		return;
+	}
+
+	// DOM manipulation adds the `.has-editable-outline` attribute to child blocks,
+	// so it needs to be removed using DOM manipulation.
+	// @see https://github.com/WordPress/gutenberg/blob/d259aab6b373789a6bb94661a67805eea7d089ba/packages/block-editor/src/components/use-flash-editable-blocks/index.js
+	const documents = [ document ];
+	document.querySelectorAll( 'iframe' ).forEach( ( iframeElement ) => {
+		try {
+			if ( iframeElement.contentDocument ) {
+				documents.push( iframeElement.contentDocument );
+			}
+		} catch {}
+	} );
+
+	clientIds.forEach( ( clientId ) => {
+		documents.forEach( ( currentDocument ) => {
+			currentDocument
+				.querySelectorAll( `[data-block="${ clientId }"]` )
+				.forEach( ( blockElement ) => {
+					blockElement.classList.remove( 'has-editable-outline' );
+				} );
+		} );
+	} );
+};
+
 function UnitoneContentOnlyLocking() {
 	const { updateBlockAttributes } = useDispatch( blockEditorStore );
 
@@ -48,10 +76,22 @@ function UnitoneContentOnlyLocking() {
 
 	const icon = ! isLocking ? lockOutline : unlock;
 
-	const onClick = () =>
+	const onClick = () => {
+		const nextTemplateLock = ! isLocking ? 'contentOnly' : undefined;
+
 		updateBlockAttributes( selectedBlock.clientId, {
-			templateLock: ! isLocking ? 'contentOnly' : undefined,
+			templateLock: nextTemplateLock,
 		} );
+
+		if ( undefined === nextTemplateLock ) {
+			const childClientIds =
+				selectedBlock?.innerBlocks?.map(
+					( innerBlock ) => innerBlock.clientId
+				) || [];
+
+			clearEditableOutlineClass( childClientIds );
+		}
+	};
 
 	return (
 		<PluginBlockSettingsMenuItem

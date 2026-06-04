db0ef523b12d7109cebfc277147998250e393b52
inc2734 <inc@2inc.org>
2026-06-04 14:18:38 +0900

    上下両端ブロックでのグループ化に失敗する不具合を修正

--- src/blocks/vertical-ends/transforms.js
+++ src/blocks/vertical-ends/transforms.js
@@ -24,7 +24,11 @@ export default {
 					);
 				} );
 
-				return createBlock( 'unitone/stack', {}, groupInnerBlocks );
+				return createBlock(
+					'unitone/vertical-ends',
+					{},
+					groupInnerBlocks
+				);
 			},
 		},
 	],

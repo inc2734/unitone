122efdd7366ab85684221fd0d1fdcf311724cbc2
inc2734 <inc@2inc.org>
2026-09-23 02:08:33 +0900

    Wrap overlay menu patterns in full-height decorators (#856)

--- patterns/overlay-menu/overlay-menu-2.php
+++ patterns/overlay-menu/overlay-menu-2.php
@@ -6,63 +6,67 @@
  * Block Types: core/template-part/unitone/overlay-menu, core/template-part/navigation-overlay
  */
 ?>
-<!-- wp:unitone/section {"tagName":"div","fontSize":"unitone-m","unitone":{"gap":"2","padding":"-1"}} -->
-<div class="has-unitone-m-font-size unitone-section">
-	<div data-unitone-layout="gutters">
-		<div data-unitone-layout="container">
-			<div data-unitone-layout="stack">
-				<!-- wp:group {"className":"unitone-overlay-menu__close","layout":{"type":"flex","justifyContent":"right"}} -->
-				<div class="wp-block-group unitone-overlay-menu__close">
-					<!-- wp:navigation-overlay-close /-->
+<!-- wp:unitone/decorator {"fontSize":"unitone-m","unitone":{"position":{"position":"relative"},"overflow":"visible","minHeight":"full","alignContent":"stretch"}} -->
+<div class="has-unitone-m-font-size" data-unitone-layout="decorator">
+	<!-- wp:unitone/section {"tagName":"div","unitone":{"gap":"2","padding":"-1"}} -->
+	<div class="unitone-section">
+		<div data-unitone-layout="gutters">
+			<div data-unitone-layout="container">
+				<div data-unitone-layout="stack">
+					<!-- wp:group {"className":"unitone-overlay-menu__close","layout":{"type":"flex","justifyContent":"right"}} -->
+					<div class="wp-block-group unitone-overlay-menu__close">
+						<!-- wp:navigation-overlay-close /-->
+					</div>
+					<!-- /wp:group -->
+
+					<!-- wp:site-logo {"align":"center"} /-->
+
+					<!-- wp:unitone/responsive-grid {"columnMinWidth":"40%","unitone":{"autoRepeat":"auto-fit","stairsUp":"left","gap":"-1"}} -->
+					<div data-unitone-layout="responsive-grid" style="--unitone--column-min-width:40%">
+						<!-- wp:buttons {"style":{"spacing":{"blockGap":{"top":"var:preset|spacing|50"}}}} -->
+						<div class="wp-block-buttons">
+							<!-- wp:button {"backgroundColor":"unitone-bright-gray","textColor":"unitone-text-black","width":100,"style":{"elements":{"link":{"color":{"text":"var:preset|color|unitone-text-black"}}}}} -->
+							<div class="wp-block-button has-custom-width wp-block-button__width-100"><a class="wp-block-button__link has-unitone-text-black-color has-unitone-bright-gray-background-color has-text-color has-background has-link-color wp-element-button">Home</a></div>
+							<!-- /wp:button -->
+						</div>
+						<!-- /wp:buttons -->
+
+						<!-- wp:buttons {"style":{"spacing":{"blockGap":{"top":"var:preset|spacing|50"}}}} -->
+						<div class="wp-block-buttons">
+							<!-- wp:button {"backgroundColor":"unitone-bright-gray","textColor":"unitone-text-black","width":100,"style":{"elements":{"link":{"color":{"text":"var:preset|color|unitone-text-black"}}}}} -->
+							<div class="wp-block-button has-custom-width wp-block-button__width-100"><a class="wp-block-button__link has-unitone-text-black-color has-unitone-bright-gray-background-color has-text-color has-background has-link-color wp-element-button">About</a></div>
+							<!-- /wp:button -->
+						</div>
+						<!-- /wp:buttons -->
+
+						<!-- wp:buttons {"style":{"spacing":{"blockGap":{"top":"var:preset|spacing|50"}}}} -->
+						<div class="wp-block-buttons">
+							<!-- wp:button {"backgroundColor":"unitone-bright-gray","textColor":"unitone-text-black","width":100,"style":{"elements":{"link":{"color":{"text":"var:preset|color|unitone-text-black"}}}}} -->
+							<div class="wp-block-button has-custom-width wp-block-button__width-100"><a class="wp-block-button__link has-unitone-text-black-color has-unitone-bright-gray-background-color has-text-color has-background has-link-color wp-element-button">Services</a></div>
+							<!-- /wp:button -->
+						</div>
+						<!-- /wp:buttons -->
+
+						<!-- wp:buttons {"style":{"spacing":{"blockGap":{"top":"var:preset|spacing|50"}}}} -->
+						<div class="wp-block-buttons">
+							<!-- wp:button {"backgroundColor":"unitone-bright-gray","textColor":"unitone-text-black","width":100,"style":{"elements":{"link":{"color":{"text":"var:preset|color|unitone-text-black"}}}}} -->
+							<div class="wp-block-button has-custom-width wp-block-button__width-100"><a class="wp-block-button__link has-unitone-text-black-color has-unitone-bright-gray-background-color has-text-color has-background has-link-color wp-element-button">Contact</a></div>
+							<!-- /wp:button -->
+						</div>
+						<!-- /wp:buttons -->
+					</div>
+					<!-- /wp:unitone/responsive-grid -->
+
+					<!-- wp:social-links {"align":"center"} -->
+					<ul class="wp-block-social-links aligncenter">
+						<!-- wp:social-link {"url":"#","service":"x"} /-->
+						<!-- wp:social-link {"url":"#","service":"facebook"} /-->
+					</ul>
+					<!-- /wp:social-links -->
 				</div>
-				<!-- /wp:group -->
-
-				<!-- wp:site-logo {"align":"center"} /-->
-
-				<!-- wp:unitone/responsive-grid {"columnMinWidth":"40%","unitone":{"autoRepeat":"auto-fit","stairsUp":"left","gap":"-1"}} -->
-				<div data-unitone-layout="responsive-grid" style="--unitone--column-min-width:40%">
-					<!-- wp:buttons {"style":{"spacing":{"blockGap":{"top":"var:preset|spacing|50"}}}} -->
-					<div class="wp-block-buttons">
-						<!-- wp:button {"backgroundColor":"unitone-bright-gray","textColor":"unitone-text-black","width":100,"style":{"elements":{"link":{"color":{"text":"var:preset|color|unitone-text-black"}}}}} -->
-						<div class="wp-block-button has-custom-width wp-block-button__width-100"><a class="wp-block-button__link has-unitone-text-black-color has-unitone-bright-gray-background-color has-text-color has-background has-link-color wp-element-button">Home</a></div>
-						<!-- /wp:button -->
-					</div>
-					<!-- /wp:buttons -->
-
-					<!-- wp:buttons {"style":{"spacing":{"blockGap":{"top":"var:preset|spacing|50"}}}} -->
-					<div class="wp-block-buttons">
-						<!-- wp:button {"backgroundColor":"unitone-bright-gray","textColor":"unitone-text-black","width":100,"style":{"elements":{"link":{"color":{"text":"var:preset|color|unitone-text-black"}}}}} -->
-						<div class="wp-block-button has-custom-width wp-block-button__width-100"><a class="wp-block-button__link has-unitone-text-black-color has-unitone-bright-gray-background-color has-text-color has-background has-link-color wp-element-button">About</a></div>
-						<!-- /wp:button -->
-					</div>
-					<!-- /wp:buttons -->
-
-					<!-- wp:buttons {"style":{"spacing":{"blockGap":{"top":"var:preset|spacing|50"}}}} -->
-					<div class="wp-block-buttons">
-						<!-- wp:button {"backgroundColor":"unitone-bright-gray","textColor":"unitone-text-black","width":100,"style":{"elements":{"link":{"color":{"text":"var:preset|color|unitone-text-black"}}}}} -->
-						<div class="wp-block-button has-custom-width wp-block-button__width-100"><a class="wp-block-button__link has-unitone-text-black-color has-unitone-bright-gray-background-color has-text-color has-background has-link-color wp-element-button">Services</a></div>
-						<!-- /wp:button -->
-					</div>
-					<!-- /wp:buttons -->
-
-					<!-- wp:buttons {"style":{"spacing":{"blockGap":{"top":"var:preset|spacing|50"}}}} -->
-					<div class="wp-block-buttons">
-						<!-- wp:button {"backgroundColor":"unitone-bright-gray","textColor":"unitone-text-black","width":100,"style":{"elements":{"link":{"color":{"text":"var:preset|color|unitone-text-black"}}}}} -->
-						<div class="wp-block-button has-custom-width wp-block-button__width-100"><a class="wp-block-button__link has-unitone-text-black-color has-unitone-bright-gray-background-color has-text-color has-background has-link-color wp-element-button">Contact</a></div>
-						<!-- /wp:button -->
-					</div>
-					<!-- /wp:buttons -->
-				</div>
-				<!-- /wp:unitone/responsive-grid -->
-
-				<!-- wp:social-links {"align":"center"} -->
-				<ul class="wp-block-social-links aligncenter">
-					<!-- wp:social-link {"url":"#","service":"x"} /-->
-					<!-- wp:social-link {"url":"#","service":"facebook"} /-->
-				</ul>
-				<!-- /wp:social-links -->
 			</div>
 		</div>
 	</div>
+	<!-- /wp:unitone/section -->
 </div>
-<!-- /wp:unitone/section -->
+<!-- /wp:unitone/decorator -->
--- patterns/overlay-menu/overlay-menu-3.php
+++ patterns/overlay-menu/overlay-menu-3.php
@@ -6,79 +6,83 @@
  * Block Types: core/template-part/unitone/overlay-menu, core/template-part/navigation-overlay
  */
 ?>
-<!-- wp:unitone/section {"tagName":"div","fontSize":"unitone-m","unitone":{"gap":"2","padding":"-1"}} -->
-<div class="has-unitone-m-font-size unitone-section">
-	<div data-unitone-layout="gutters">
-		<div data-unitone-layout="container">
-			<div data-unitone-layout="stack">
-				<!-- wp:group {"className":"unitone-overlay-menu__close","layout":{"type":"flex","justifyContent":"right"}} -->
-				<div class="wp-block-group unitone-overlay-menu__close">
-					<!-- wp:navigation-overlay-close /-->
-				</div>
-				<!-- /wp:group -->
+<!-- wp:unitone/decorator {"fontSize":"unitone-m","unitone":{"position":{"position":"relative"},"overflow":"visible","minHeight":"full","alignContent":"stretch"}} -->
+<div class="has-unitone-m-font-size" data-unitone-layout="decorator">
+	<!-- wp:unitone/section {"tagName":"div","unitone":{"gap":"2","padding":"-1"}} -->
+	<div class="unitone-section">
+		<div data-unitone-layout="gutters">
+			<div data-unitone-layout="container">
+				<div data-unitone-layout="stack">
+					<!-- wp:group {"className":"unitone-overlay-menu__close","layout":{"type":"flex","justifyContent":"right"}} -->
+					<div class="wp-block-group unitone-overlay-menu__close">
+						<!-- wp:navigation-overlay-close /-->
+					</div>
+					<!-- /wp:group -->
 
-				<!-- wp:site-logo {"align":"center"} /-->
+					<!-- wp:site-logo {"align":"center"} /-->
 
-				<!-- wp:unitone/grid {"smColumnsOption":"columns","smColumns":2,"unitone":{"gap":{"column":"1","row":"-2"}},"style":{"typography":{"fontStyle":"normal","fontWeight":"700"}},"fontSize":"unitone-xl"} -->
-				<div class="unitone-grid has-unitone-xl-font-size" style="font-style:normal;font-weight:700;--unitone--column-min-width:240px;--unitone--column-auto-repeat:auto-fit;--unitone--sm-columns:2;--unitone--rows:1" data-unitone-layout="-columns:min -columns:sm:columns -rows:rows">
-					<!-- wp:buttons {"style":{"spacing":{"blockGap":{"top":"var:preset|spacing|20","left":"var:preset|spacing|30"}}},"layout":{"type":"flex","justifyContent":"space-between"}} -->
-					<div class="wp-block-buttons">
-						<!-- wp:button {"textColor":"unitone-inherit","className":"is-style-text","style":{"border":{"width":"0px","style":"none"},"elements":{"link":{"color":{"text":"var:preset|color|unitone-inherit"}}}}} -->
-						<div class="wp-block-button is-style-text"><a class="wp-block-button__link has-unitone-inherit-color has-text-color has-link-color wp-element-button" style="border-style:none;border-width:0px">- Home</a></div>
+					<!-- wp:unitone/grid {"smColumnsOption":"columns","smColumns":2,"unitone":{"gap":{"column":"1","row":"-2"}},"style":{"typography":{"fontStyle":"normal","fontWeight":"700"}},"fontSize":"unitone-xl"} -->
+					<div class="unitone-grid has-unitone-xl-font-size" style="font-style:normal;font-weight:700;--unitone--column-min-width:240px;--unitone--column-auto-repeat:auto-fit;--unitone--sm-columns:2;--unitone--rows:1" data-unitone-layout="-columns:min -columns:sm:columns -rows:rows">
+						<!-- wp:buttons {"style":{"spacing":{"blockGap":{"top":"var:preset|spacing|20","left":"var:preset|spacing|30"}}},"layout":{"type":"flex","justifyContent":"space-between"}} -->
+						<div class="wp-block-buttons">
+							<!-- wp:button {"textColor":"unitone-inherit","className":"is-style-text","style":{"border":{"width":"0px","style":"none"},"elements":{"link":{"color":{"text":"var:preset|color|unitone-inherit"}}}}} -->
+							<div class="wp-block-button is-style-text"><a class="wp-block-button__link has-unitone-inherit-color has-text-color has-link-color wp-element-button" style="border-style:none;border-width:0px">- Home</a></div>
+							<!-- /wp:button -->
+						</div>
+						<!-- /wp:buttons -->
+
+						<!-- wp:buttons {"style":{"spacing":{"blockGap":{"top":"var:preset|spacing|20","left":"var:preset|spacing|30"}}},"layout":{"type":"flex","justifyContent":"space-between"}} -->
+						<div class="wp-block-buttons">
+							<!-- wp:button {"textColor":"unitone-inherit","className":"is-style-text","style":{"border":{"width":"0px","style":"none"},"elements":{"link":{"color":{"text":"var:preset|color|unitone-inherit"}}}}} -->
+							<div class="wp-block-button is-style-text"><a class="wp-block-button__link has-unitone-inherit-color has-text-color has-link-color wp-element-button" style="border-style:none;border-width:0px">- About</a></div>
+							<!-- /wp:button -->
+						</div>
+						<!-- /wp:buttons -->
+
+						<!-- wp:buttons {"style":{"spacing":{"blockGap":{"top":"var:preset|spacing|20","left":"var:preset|spacing|30"}}},"layout":{"type":"flex","justifyContent":"space-between"}} -->
+						<div class="wp-block-buttons">
+							<!-- wp:button {"textColor":"unitone-inherit","className":"is-style-text","style":{"border":{"width":"0px","style":"none"},"elements":{"link":{"color":{"text":"var:preset|color|unitone-inherit"}}}}} -->
+							<div class="wp-block-button is-style-text"><a class="wp-block-button__link has-unitone-inherit-color has-text-color has-link-color wp-element-button" style="border-style:none;border-width:0px">- Services</a></div>
+						<!-- /wp:button -->
+						</div>
+						<!-- /wp:buttons -->
+
+						<!-- wp:buttons {"style":{"spacing":{"blockGap":{"top":"var:preset|spacing|20","left":"var:preset|spacing|30"}}},"layout":{"type":"flex","justifyContent":"space-between"}} -->
+						<div class="wp-block-buttons">
+							<!-- wp:button {"textColor":"unitone-inherit","className":"is-style-text","style":{"border":{"width":"0px","style":"none"},"elements":{"link":{"color":{"text":"var:preset|color|unitone-inherit"}}}}} -->
+							<div class="wp-block-button is-style-text"><a class="wp-block-button__link has-unitone-inherit-color has-text-color has-link-color wp-element-button" style="border-style:none;border-width:0px">- News</a></div>
+						<!-- /wp:button -->
+						</div>
+						<!-- /wp:buttons -->
+
+						<!-- wp:buttons {"style":{"spacing":{"blockGap":{"top":"var:preset|spacing|20","left":"var:preset|spacing|30"}}},"layout":{"type":"flex","justifyContent":"space-between"}} -->
+						<div class="wp-block-buttons">
+							<!-- wp:button {"textColor":"unitone-inherit","className":"is-style-text","style":{"border":{"width":"0px","style":"none"},"elements":{"link":{"color":{"text":"var:preset|color|unitone-inherit"}}}}} -->
+							<div class="wp-block-button is-style-text"><a class="wp-block-button__link has-unitone-inherit-color has-text-color has-link-color wp-element-button" style="border-style:none;border-width:0px">- Recruit</a></div>
+						<!-- /wp:button -->
+						</div>
+						<!-- /wp:buttons -->
+					</div>
+					<!-- /wp:unitone/grid -->
+
+					<!-- wp:buttons {"style":{"typography":{"fontStyle":"normal","fontWeight":"700"}}} -->
+					<div class="wp-block-buttons" style="font-style:normal;font-weight:700">
+						<!-- wp:button {"textColor":"unitone-inherit","width":100,"className":"is-style-outline","style":{"elements":{"link":{"color":{"text":"var:preset|color|unitone-inherit"}}}},"borderColor":"unitone-current"} -->
+						<div class="wp-block-button has-custom-width wp-block-button__width-100 is-style-outline"><a class="wp-block-button__link has-unitone-inherit-color has-text-color has-link-color has-border-color has-unitone-current-border-color wp-element-button">Contact</a></div>
 						<!-- /wp:button -->
 					</div>
 					<!-- /wp:buttons -->
 
-					<!-- wp:buttons {"style":{"spacing":{"blockGap":{"top":"var:preset|spacing|20","left":"var:preset|spacing|30"}}},"layout":{"type":"flex","justifyContent":"space-between"}} -->
-					<div class="wp-block-buttons">
-						<!-- wp:button {"textColor":"unitone-inherit","className":"is-style-text","style":{"border":{"width":"0px","style":"none"},"elements":{"link":{"color":{"text":"var:preset|color|unitone-inherit"}}}}} -->
-						<div class="wp-block-button is-style-text"><a class="wp-block-button__link has-unitone-inherit-color has-text-color has-link-color wp-element-button" style="border-style:none;border-width:0px">- About</a></div>
-						<!-- /wp:button -->
-					</div>
-					<!-- /wp:buttons -->
-
-					<!-- wp:buttons {"style":{"spacing":{"blockGap":{"top":"var:preset|spacing|20","left":"var:preset|spacing|30"}}},"layout":{"type":"flex","justifyContent":"space-between"}} -->
-					<div class="wp-block-buttons">
-						<!-- wp:button {"textColor":"unitone-inherit","className":"is-style-text","style":{"border":{"width":"0px","style":"none"},"elements":{"link":{"color":{"text":"var:preset|color|unitone-inherit"}}}}} -->
-						<div class="wp-block-button is-style-text"><a class="wp-block-button__link has-unitone-inherit-color has-text-color has-link-color wp-element-button" style="border-style:none;border-width:0px">- Services</a></div>
-					<!-- /wp:button -->
-					</div>
-					<!-- /wp:buttons -->
-
-					<!-- wp:buttons {"style":{"spacing":{"blockGap":{"top":"var:preset|spacing|20","left":"var:preset|spacing|30"}}},"layout":{"type":"flex","justifyContent":"space-between"}} -->
-					<div class="wp-block-buttons">
-						<!-- wp:button {"textColor":"unitone-inherit","className":"is-style-text","style":{"border":{"width":"0px","style":"none"},"elements":{"link":{"color":{"text":"var:preset|color|unitone-inherit"}}}}} -->
-						<div class="wp-block-button is-style-text"><a class="wp-block-button__link has-unitone-inherit-color has-text-color has-link-color wp-element-button" style="border-style:none;border-width:0px">- News</a></div>
-					<!-- /wp:button -->
-					</div>
-					<!-- /wp:buttons -->
-
-					<!-- wp:buttons {"style":{"spacing":{"blockGap":{"top":"var:preset|spacing|20","left":"var:preset|spacing|30"}}},"layout":{"type":"flex","justifyContent":"space-between"}} -->
-					<div class="wp-block-buttons">
-						<!-- wp:button {"textColor":"unitone-inherit","className":"is-style-text","style":{"border":{"width":"0px","style":"none"},"elements":{"link":{"color":{"text":"var:preset|color|unitone-inherit"}}}}} -->
-						<div class="wp-block-button is-style-text"><a class="wp-block-button__link has-unitone-inherit-color has-text-color has-link-color wp-element-button" style="border-style:none;border-width:0px">- Recruit</a></div>
-					<!-- /wp:button -->
-					</div>
-					<!-- /wp:buttons -->
+					<!-- wp:social-links {"align":"center"} -->
+					<ul class="wp-block-social-links aligncenter">
+						<!-- wp:social-link {"url":"#","service":"x"} /-->
+						<!-- wp:social-link {"url":"#","service":"facebook"} /-->
+					</ul>
+					<!-- /wp:social-links -->
 				</div>
-				<!-- /wp:unitone/grid -->
-
-				<!-- wp:buttons {"style":{"typography":{"fontStyle":"normal","fontWeight":"700"}}} -->
-				<div class="wp-block-buttons" style="font-style:normal;font-weight:700">
-					<!-- wp:button {"textColor":"unitone-inherit","width":100,"className":"is-style-outline","style":{"elements":{"link":{"color":{"text":"var:preset|color|unitone-inherit"}}}},"borderColor":"unitone-current"} -->
-					<div class="wp-block-button has-custom-width wp-block-button__width-100 is-style-outline"><a class="wp-block-button__link has-unitone-inherit-color has-text-color has-link-color has-border-color has-unitone-current-border-color wp-element-button">Contact</a></div>
-					<!-- /wp:button -->
-				</div>
-				<!-- /wp:buttons -->
-
-				<!-- wp:social-links {"align":"center"} -->
-				<ul class="wp-block-social-links aligncenter">
-					<!-- wp:social-link {"url":"#","service":"x"} /-->
-					<!-- wp:social-link {"url":"#","service":"facebook"} /-->
-				</ul>
-				<!-- /wp:social-links -->
 			</div>
 		</div>
 	</div>
+	<!-- /wp:unitone/section -->
 </div>
-<!-- /wp:unitone/section -->
+<!-- /wp:unitone/decorator -->
